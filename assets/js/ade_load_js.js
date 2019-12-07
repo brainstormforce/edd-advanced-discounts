d39bf89cec3e363df01c89457ae8305a2e7cf986
David Rozando <david.rozando@gmail.com>
2019-12-08 02:55:41 +0700

    Use tab indentation

--- assets/js/ade_load_js.js
+++ assets/js/ade_load_js.js
@@ -3,12 +3,12 @@
  */
 
 jQuery(document).ready(function(){
-    jQuery( '#product_request' ).change( function() {
-    	var conditions = jQuery( '#edd-discount-product-conditions_new' );
-    	if( $(this).val() ) {
-		conditions.show();
-	} else {
-		conditions.hide();
-	}
-    });
- });
+	jQuery( '#product_request' ).change( function() {
+		var conditions = jQuery( '#edd-discount-product-conditions_new' );
+		if( $(this).val() ) {
+			conditions.show();
+		} else {
+			conditions.hide();
+		}
+	});
+});
