c59ec83acbc308470ab7eafea66edd49d7a319a3
ganeshk@bsf.io <ganeshk@bsf.io>
2020-12-30 15:14:14 +0530

    removed updated version

--- edd-advanced-discounts.php
+++ edd-advanced-discounts.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: EDD Advanced Discounts
  * Description: Customize EDD discounts easily that will bring more sales and profit to your store. Offer discounts on selected product variations and set a maximum limit for cart total.
- * Version:     1.0.3
+ * Version:     1.0.2
  * Author:      Brainstorm Force
  * Author URI:  https://brainstormforce.com
  * Text Domain: advanced-discount-edd
@@ -14,7 +14,7 @@
  * @link     https://brainstormforce.com
  */
 
-define( 'ADE_EDD_VER', '1.0.3' );
+define( 'ADE_EDD_VER', '1.0.2' );
 define( 'ADE_EDD_ABSPATH', plugin_dir_path( __FILE__ ) );
 define( 'ADE_EDD_PLUGIN_URL', untrailingslashit( plugins_url( '', __FILE__ ) ) );
 define( 'ADE_EDD_PLUGIN_DIR', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
